bf0e909ae91eb8b906546bfdaaf3d6b08ac7b85e
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-06-27 13:11:13 +0300

    update

--- script/3-1-tabs.js
+++ script/3-1-tabs.js
@@ -1,4 +1,11 @@
 'use strict';
+
+
+// САМ НЕ СДЕЛАЛ!!!! 
+
+
+
+
 const texts = {
 	text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
 	text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
@@ -12,6 +19,13 @@ const texts = {
 	 обработчик клика функцию clickHandler.
 */
 
+let textEl = document.querySelector('.text');
+let navLinkEl = document.querySelectorAll('.nav-link');
+
+navLinkEl.forEach(link => {
+	link.addEventListener('click', clickHandler)
+});
+
 
 /**
  * Обработчик клика по .nav-link
@@ -20,7 +34,8 @@ const texts = {
 function clickHandler(event) {
 	// здесь вызывайте changeText и changeActiveClass, и передавайте
 	// им объект события.
-
+	changeText(event);
+	changeActiveClass(event);
 }
 
 /**
@@ -29,7 +44,8 @@ function clickHandler(event) {
  * @param {MouseEvent} event 
  */
 function changeActiveClass(event) {
-
+	document.querySelector('.active').classList.remove('active');
+	event.target.classList.add('active');
 }
 
 /**
@@ -39,5 +55,15 @@ function changeActiveClass(event) {
  * @param {MouseEvent} event 
  */
 function changeText(event) {
-
+	switch (event.target.textContent) {
+		case "Link 1":
+			textEl.textContent = texts.text1;
+			break;
+		case "Link 2":
+			textEl.textContent = texts.text2;
+			break;
+		case "Link 3":
+			textEl.textContent = texts.text3;
+			break;
+	}
 }
