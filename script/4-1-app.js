12f4b485e4ca843d130240923cbb72bc017d37e0
Zaharcheniavitaly <zaharchenyavitaly@yandex.by>
2022-06-27 16:06:40 +0300

    Update 4-1-app.js

--- script/4-1-app.js
+++ script/4-1-app.js
@@ -1,79 +1,98 @@
 "use strict";
+
+
+// САМ НЕ СДЕЛАЛ!!!!!
+
+/*
+Задание. Вам нужно при кликах по кнопкам показывать соответствующие товары.
+	 Информация о товарах находится в объекте products в файле app.js. Для генерации
+	 разметки можете использовать шаблон представленный в app.js.
+*/
+
+
 /* 
 Разметка товара:
 
 <div class="product">
-    <div>${здесь_название_товара}</div>
-    <img src="${здесь путь до картинки}" alt="">
-    <div>${здесь_цена}</div>
-    <a href="https://example.com/producs/${здесь_id_товара}">Подробнее</a>
+	 <div>${здесь_название_товара}</div>
+	 <img src="${здесь путь до картинки}" alt="">
+	 <div>${здесь_цена}</div>
+	 <a href="https://example.com/producs/${здесь_id_товара}">Подробнее</a>
 </div>
 */
 
 const products = {
-    phones: [
-        {
-            id: 1,
-            name: "Смартфон 1",
-            price: "23,99 р.",
-            imageUrl: "https://picsum.photos/seed/1/200",
-        },
-        {
-            id: 2,
-            name: "Смартфон 2",
-            price: "11,99 р.",
-            imageUrl: "https://picsum.photos/seed/2/200",
-        },
-        {
-            id: 3,
-            name: "Смартфон 3",
-            price: "22,99 р.",
-            imageUrl: "https://picsum.photos/seed/3/200",
-        },
-    ],
+	phones: [
+		{
+			id: 1,
+			name: "Смартфон 1",
+			price: "23,99 р.",
+			imageUrl: "https://picsum.photos/seed/1/200",
+		},
+		{
+			id: 2,
+			name: "Смартфон 2",
+			price: "11,99 р.",
+			imageUrl: "https://picsum.photos/seed/2/200",
+		},
+		{
+			id: 3,
+			name: "Смартфон 3",
+			price: "22,99 р.",
+			imageUrl: "https://picsum.photos/seed/3/200",
+		},
+	],
 
-    tablets: [
-        {
-            id: 4,
-            name: "Планшет 4",
-            price: "99,99 р.",
-            imageUrl: "https://picsum.photos/seed/4/200",
-        },
-        {
-            id: 5,
-            name: "Планшет 5",
-            price: "44,99 р.",
-            imageUrl: "https://picsum.photos/seed/5/200",
-        },
-    ],
+	tablets: [
+		{
+			id: 4,
+			name: "Планшет 4",
+			price: "99,99 р.",
+			imageUrl: "https://picsum.photos/seed/4/200",
+		},
+		{
+			id: 5,
+			name: "Планшет 5",
+			price: "44,99 р.",
+			imageUrl: "https://picsum.photos/seed/5/200",
+		},
+	],
 
-    tv: [
-        {
-            id: 6,
-            name: "Телевизор 6",
-            price: "199,99 р.",
-            imageUrl: "https://picsum.photos/seed/6/200",
-        },
-        {
-            id: 7,
-            name: "Телевизор 7",
-            price: "244,99 р.",
-            imageUrl: "https://picsum.photos/seed/7/200",
-        },
-        {
-            id: 8,
-            name: "Телевизор 8",
-            price: "399,99 р.",
-            imageUrl: "https://picsum.photos/seed/8/200",
-        },
-        {
-            id: 9,
-            name: "Телевизор 9",
-            price: "444,99 р.",
-            imageUrl: "https://picsum.photos/seed/9/200",
-        },
-    ],
+	tv: [
+		{
+			id: 6,
+			name: "Телевизор 6",
+			price: "199,99 р.",
+			imageUrl: "https://picsum.photos/seed/6/200",
+		},
+		{
+			id: 7,
+			name: "Телевизор 7",
+			price: "244,99 р.",
+			imageUrl: "https://picsum.photos/seed/7/200",
+		},
+		{
+			id: 8,
+			name: "Телевизор 8",
+			price: "399,99 р.",
+			imageUrl: "https://picsum.photos/seed/8/200",
+		},
+		{
+			id: 9,
+			name: "Телевизор 9",
+			price: "444,99 р.",
+			imageUrl: "https://picsum.photos/seed/9/200",
+		},
+	],
 };
+
+
+
+let productsEl = document.querySelector('.products');
+let buttonEl = document.querySelectorAll('button');
+buttonEl.forEach(button => {
+	button.addEventListener('click', clickHandler)
+});
 
 
 /**
@@ -81,11 +100,11 @@ const products = {
  * @param {MouseEvent} event
  */
 function clickHandler(event) {
-    //вам нужно очищать содержимое .products
-    
-    //в showCategory надо передать строку с типом категории, тип берите
-    //из атрибута data-type у кнопки, по которой кликнули.
-    
+	//вам нужно очищать содержимое .products
+	productsEl.innerHTML = "";
+	//в showCategory надо передать строку с типом категории, тип берите
+	//из атрибута data-type у кнопки, по которой кликнули.
+	showCategory(event.target.dataset.type);
 }
 
 /**
@@ -96,7 +115,12 @@ function clickHandler(event) {
  * по которой кликнули.
  */
 function showCategory(category) {
-    
+	const categoryProducts = products[category];
+	let markupToProductsDiv = "";
+	categoryProducts.forEach(function (product) {
+		markupToProductsDiv += getProductMarkup(product);
+	});
+	productsEl.insertAdjacentHTML("afterbegin", markupToProductsDiv);
 }
 
 /**
@@ -108,6 +132,15 @@ function showCategory(category) {
  * @returns {string} html-разметка для товара по аналогии из комментария
  * в верху этого файла.
  */
-function getProductMarkup(product) {
 
+
+function getProductMarkup(product) {
+	return `
+<div class="product">
+	<div>${product.name}</div>
+	<img src="${product.imageUrl}" alt="">
+	<div>${product.price}</div>
+	<a href="https://example.com/producs/${product.id}">Подробнее</a>
+</div>
+`;
 }
